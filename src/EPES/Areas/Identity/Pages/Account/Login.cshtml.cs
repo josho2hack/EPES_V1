d80b2052f8fe16e5ee1d1bc95324459a05a74b5b
josho2hack <josho2hack@gmail.com>
2019-05-04 20:57:33 +0700

    Update UI

--- src/EPES/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/EPES/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,7 +44,7 @@ namespace EPES.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "บันทึกการเข้าสู่ระบบไว้จนกว่าจะคลิกออกจากระบบ ?")]
             public bool RememberMe { get; set; }
         }
 
