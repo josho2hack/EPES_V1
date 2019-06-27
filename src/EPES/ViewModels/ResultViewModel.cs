20fb074d9b7f73354b12627236880d5a68f9b3cd
josho2hack <josho2hack@gmail.com>
2019-06-27 08:20:07 +0700

    create result modelview

---
+++ src/EPES/ViewModels/ResultViewModel.cs
@@ -0,0 +1,24 @@
+﻿using EPES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPES.ViewModels
+{
+    public class ResultViewModel
+    {
+        public IEnumerable<PointOfEvaluation> pointA { get; set; }
+        public IEnumerable<PointOfEvaluation> pointB { get; set; }
+        public IEnumerable<PointOfEvaluation> pointC { get; set; }
+        public IEnumerable<PointOfEvaluation> pointD { get; set; }
+
+        public IEnumerable<Office> Offices { get; set; }
+
+        public Office Office { get; set; }
+        public PointOfEvaluation Point { get; set; }
+
+        public int yearPoint { get; set; }
+        public int poeid { get; set; }
+    }
+}
