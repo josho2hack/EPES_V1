23e073db105f20f6d81e1c291c5590a6a67fa000
Josho Hack <josho2hack@gmail.com>
2021-09-10 15:06:31 +0700

    update owner report

--- src/EPES/Controllers/OwnerScoreController.cs
+++ src/EPES/Controllers/OwnerScoreController.cs
@@ -65,64 +65,9 @@ namespace EPES.Controllers
                 }
             }
 
-            var scoreDrafts = _context.ScoreDrafts.Include(p=>p.PointOfEvaluation).Where(sd => sd.Office.Code == office && sd.PointOfEvaluation.SubPoint == 0 && sd.PointOfEvaluation.Year == yearForQuery)
-                                                    .Select(i => new
-                                                    {
-                                                        i.Id,
-                                                        i.PointOfEvaluation.Plan,
-                                                        i.PointOfEvaluation.Point,
-                                                        i.PointOfEvaluation.SubPoint,
-                                                        i.PointOfEvaluation.Name,
-                                                        i.PointOfEvaluation.Weight,
-                                                        i.ScoreValue,
-                                                        i.ScoreApprove,
-                                                        i.LastMonth,
-                                                        i.PointOfEvaluation.Year.Year,
-                                                        cal = (i.PointOfEvaluation.Weight * i.ScoreValue /100 ),
-                                                        calApprove = (i.PointOfEvaluation.Weight * i.ScoreApprove / 100)
-                                                    });
+            var scoreDrafts = _context.ScoreDrafts.Include(p => p.PointOfEvaluation).Include(d => d.PointOfEvaluation.DataForEvaluations).Where(sd => sd.Office.Code == office && sd.PointOfEvaluation.SubPoint == 0 && sd.PointOfEvaluation.Year == yearForQuery);
 
-            return Json(await DataSourceLoader.LoadAsync(scoreDrafts, loadOptions));
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetSub(DataSourceLoadOptions loadOptions, string selectoffice, int yearPoint)
-        {
-            var user = await _userManager.GetUserAsync(User);
-            string office;
-            if (selectoffice == null)
-            {
-                office = user.OfficeId;
-            }
-            else
-            {
-                office = selectoffice;
-            }
-            DateTime yearForQuery;
-            if (yearPoint == 0)
-            {
-                if (DateTime.Now.Month == 10 || DateTime.Now.Month == 11 || DateTime.Now.Month == 12)
-                {
-                    yearForQuery = new DateTime(DateTime.Now.AddYears(1).Year, 1, 1);
-                }
-                else
-                {
-                    yearForQuery = new DateTime(DateTime.Now.Year, 1, 1);
-                }
-            }
-            else
-            {
-                if (DateTime.Now.Month == 10 || DateTime.Now.Month == 11 || DateTime.Now.Month == 12)
-                {
-                    yearForQuery = new DateTime(DateTime.Now.AddYears(1 + yearPoint).Year, 1, 1);
-                }
-                else
-                {
-                    yearForQuery = new DateTime(DateTime.Now.AddYears(yearPoint).Year, 1, 1);
-                }
-            }
-
-            var scoreDrafts = _context.ScoreDrafts.Include(p => p.PointOfEvaluation).Include(d => d.PointOfEvaluation.DataForEvaluations).Where(sd => sd.Office.Code == office && sd.PointOfEvaluation.SubPoint == 0 && sd.PointOfEvaluation.Year == yearForQuery);                                                  
+            //List<ScoreDraft> scoreTemp = new List<ScoreDraft>();
 
             foreach (var item in scoreDrafts)
             {
@@ -136,6 +81,12 @@ namespace EPES.Controllers
                         }
                     }
                 }
+                else
+                {
+                    item.weightOfMonth = item.PointOfEvaluation.Weight;
+                }
+
+                //scoreTemp.Add(item);
             }
 
             var score = scoreDrafts.Select(i => new
@@ -145,19 +96,17 @@ namespace EPES.Controllers
                 i.PointOfEvaluation.Point,
                 i.PointOfEvaluation.SubPoint,
                 i.PointOfEvaluation.Name,
-                i.PointOfEvaluation.Weight,
                 i.ScoreValue,
                 i.ScoreApprove,
                 i.LastMonth,
                 i.PointOfEvaluation.Year.Year,
-                cal = (i.weightOfMonth * i.ScoreValue / 100),
-                calApprove = (i.weightOfMonth * i.ScoreApprove / 100),
+                cal = !i.PointOfEvaluation.WeightAll ? (i.weightOfMonth * i.ScoreValue / 100) : (i.PointOfEvaluation.Weight * i.ScoreValue / 100),
+                calApprove = !i.PointOfEvaluation.WeightAll ?  (i.weightOfMonth * i.ScoreApprove / 100) : (i.PointOfEvaluation.Weight * i.ScoreValue / 100),
                 i.weightOfMonth
             });
 
 
-
-            return Json(await DataSourceLoader.LoadAsync(scoreDrafts, loadOptions));
+            return Json(await DataSourceLoader.LoadAsync(score, loadOptions));
         }
     }
 }
